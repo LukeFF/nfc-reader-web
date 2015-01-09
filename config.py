3f50293ff859b3d1d7b9351663c21aa4502f06e4
Lukas Thoma <lukasthoma@gmail.com>
2015-01-09 16:29:21 +0100

    now working

--- config.py
+++ config.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import os
 
-default_device = 'tty:AMA0:pn53x'
+default_device = ['tty:AMA0:pn53x']
 #default_path = os.path.dirname(os.path.abspath(__file__)) + '/export'
-default_path = 'http://192.168.1.100:8000'
-default_format = 'xml'
+default_path = 'http://192.168.1.100:8000/tags/'
